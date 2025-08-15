fb5bdc867e7c2315aa0f1163b903a3092a9c7f30
villno55 <adsotrabajos24@gmail.com>
2025-08-15 16:10:01 -0500

    Ejercicio1.js

--- problem1.js
+++ problem1.js
@@ -1,16 +1,9 @@
 /*Escribir un script que diga si un número es par o es impar. Recordemos que un número es par
 si al dividirlo por 2 da como resto 0.*/
 
-// Verificar si un número es par o impar
-
-const prompt = require('prompt-sync')();  
-
 let numero = parseInt(prompt("Ingrese un número:"));
-
-if (isNaN(numero)) {
-  console.log("Eso no es un número válido.");
-} else if (numero % 2 === 0) {
-  console.log(numero + " es par.");
+if (numero % 2 === 0) {
+    console.log(`${numero} es par`);
 } else {
-  console.log(numero + " es impar.");
+    console.log(`${numero} es impar`);
 }
