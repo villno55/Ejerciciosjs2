aec860797f4730e5335cc4ef56baffd601e78565
villno55 <adsotrabajos24@gmail.com>
2025-08-15 16:23:26 -0500

    Update and rename problem5.js to Ejercicio5.js

---
+++ Ejercicio5.js
@@ -0,0 +1,8 @@
+let edadCliente = parseInt(prompt("Ingresa tu edad:"));
+if (edadCliente < 5) {
+    console.log("Entras gratis");
+} else if (edadCliente <= 18) {
+    console.log("El precio de la entrada es de 5 mil pesos");
+} else {
+    console.log("El precio de la entrada es de 10 mil pesos");
+}
