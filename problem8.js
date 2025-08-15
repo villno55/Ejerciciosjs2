92591752504b8f58d8b95da5792d3aef38c4b7ce
villno55 <adsotrabajos24@gmail.com>
2025-08-15 16:32:00 -0500

    Ejercicio8.js

--- problem8.js
+++ problem8.js
@@ -1,12 +1,15 @@
-/*Una torta de papa lleva 200 gramos de papa por persona. Por cada kilo de papa se necesitan 5
-huevos y 300 gramos de cebolla. Escribe un script que dado el número de personas calcule las
-cantidades de ingredientes necesarias */ 
+let num = parseInt(prompt("Ingrese un número:"));
+let Primo = num > 1;
 
-let personas = parseInt(prompt("Ingrese el número de personas:"));
-let PapaGramos = personas * 200;
-let kiPapa = PapaGramos / 1000;
-let huevos = kiPapa * 5;
-let gramosCebolla = kiPapa * 300;
-console.log("Papa: " + PapaGramos + " gramos");
-console.log("Huevos: " + Math.ceil(huevos));
-console.log("Cebolla: " + gramosCebolla + " gramos");
+for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+        Primo = false;
+        break;
+    }
+}
+
+if (Primo) {
+    console.log(`El numero ${num} es primo`);
+} else {
+    console.log(`El numero ${num} no es primo`);
+}
