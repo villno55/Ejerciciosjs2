6d8232e5ef9fd2c5f513c39279dfadc6df63980c
villno55 <adsotrabajos24@gmail.com>
2025-08-15 16:28:50 -0500

    Ejercicio7.js

--- problem7.js
+++ problem7.js
@@ -1,10 +1,8 @@
-/*En este script debe pedir al usuario un número de dos dígitos y debe devolver el número de
-unidades y de decenas, o sea, cada dígito del número. Usa solo operaciones aritméticas.
-Recuerda si divides un número entre 10 el cociente entero es el número de decenas y el resto
-es el número de unidades*/
+let ini = parseInt(prompt("Ingresa el primer número:"));
+let fin = parseInt(prompt("Ingresa el otro número:"));
 
-let numero = parseInt(prompt("Ingresa un número de dos dígitos:"));
-let decenas = Math.floor(numero / 10);
-let unidades = numero % 10;
-console.log("Decenas: " + decenas);
-console.log("Unidades: " + unidades);
+for (let i = ini; i <= fin; i++) {
+    if (i % 3 === 0) {
+        console.log(i);
+    }
+}
